5bae5d59b4b31e32c28c9269d89040e31d769ce6
shaunregenbaum <shaunregenbaum@gmail.com>
2022-11-10 15:46:50 +0200

    earth in array is added

---
+++ src/components/animations/world.tsx
@@ -0,0 +1,34 @@
+import { component$ } from "@builder.io/qwik";
+
+
+const earth_lines=[
+    `              _-o#&&*''''?d:>bl_`,
+    `          _o/"'''  '',, dMF9MMMMMHo_`,
+`       .o&#'        '"MbHMMMMMMMMMMMHo.`,
+`     .o"" '         vodM*$&&HMMMMMMMMMM?.`,
+`    ,'              $M&ood,~''(&##MMMMMMHL`,
+`   /               ,MMMMMMM#b?#bobMMMMHMMML`,
+`  &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk`,
+` ?$.            :MMMMMMMMMMMMMMMMMMM/HMMM|'*L`,
+`|               |MMMMMMMMMMMMMMMMMMMMbMH'   T`,
+`$H#:            '*MMMMMMMMMMMMMMMMMMMMb#}'  '?`,
+`]MMH#             ""*""""*#MMMMMMMMMMMMM'    -`,
+`MMMMMb_                   |MMMMMMMMMMMP'     :`,
+`HMMMMMMMHo                 'MMMMMMMMMT       .`,
+`?MMMMMMMMP                  9MMMMMMMM}       -`,
+`-?MMMMMMM                  |MMMMMMMMM?,d-    `,
+`:|MMMMMM-                 'MMMMMMMT .M|.   :`,
+`  .9MMM[                    &MMMMM*' ''    .`,
+`:9MMk                    'MMM#"        -`,
+`     &M}                     '          .-`,
+`      '&.                             .`,
+`        '~,   .                     ./`,
+`            . _                  .-`,
+`              ''--._,dd###pp=""'`,
+]
+export default component$(() => {
+     return(
+            <div>
+                </div>
+     )
+})
