8583cad005b3de24ede46a6f4366284381cabf44
shaunregenbaum <shaunregenbaum@gmail.com>
2022-11-13 10:28:56 +0200

    using three js

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { Link } from '@builder.io/qwik-city';
+import World from '../components/animations/world';
 
 export default component$(() => {
   return (
     <div>
+      <World />
       <h1>
         Welcome to Qwik <span class="lightning">⚡️</span>
       </h1>
