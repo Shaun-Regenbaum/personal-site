a9433542ef4ce03ac199578e3befacdc473a03a1
shaunregenbaum <shaunregenbaum@gmail.com>
2022-12-03 21:29:27 +0200

    hein quote

---
+++ src/routes/err.tsx
@@ -0,0 +1,22 @@
+export default function Err() {
+	return (
+		<div class='bg-gray-800 h-screen w-full pl-12 pt-12'>
+			<h1 class='text-blue-200 text-5xl'>What is the road to wisdom?</h1>
+			<br></br>
+			<p class='text-3xl text-blue-200'>Well, its plain</p>
+			<p class='text-3xl text-blue-200'>And simple to express:</p>
+			<br></br>
+			<p class='text-3xl text-blue-200'>Err</p>
+			<p class='text-3xl text-blue-200'>And Err</p>
+			<p class='text-3xl text-blue-200'>And Err again</p>
+			<br></br>
+			<p class='text-3xl text-blue-200'>But less</p>
+			<p class='text-3xl text-blue-200'>and less</p>
+			<p class='text-3xl text-blue-200'>and less.</p>
+			<br></br>
+			<br></br>
+			<p class=' text-blue-200'> ___ </p>
+			<p class=' text-blue-200'>Piet Hein</p>
+		</div>
+	);
+}
---
+++ src/routes/td.tsx
@@ -0,0 +1,8 @@
+
+export default function TD() {
+	return (
+		<main class='mx-auto text-gray-700 p-4'>
+			<h1>Hey I am Shaun Regenbaum</h1>
+		</main>
+	);
+}
