37b93e8b37df9cbfcf0d7ad6208f35a69df45b21
shaunregenbaum <shaunregenbaum@gmail.com>
2022-12-03 22:40:19 +0200

    finished hein poem, need to find cool fonts at some point

--- src/routes/err.tsx
+++ src/routes/err.tsx
@@ -1,18 +1,30 @@
 export default function Err() {
+	// the tab special charecter in html:
 	return (
-		<div class='bg-gray-800 h-screen w-full pl-12 pt-12'>
-			<h1 class='text-blue-200 text-5xl'>What is the road to wisdom?</h1>
+		<div class='bg-gray-800 h-screen w-full pl-12 pr-4 pt-12 font-serif'>
+			<h1 class='text-blue-200 text-5xl'>What Is The Road To Wisdom?</h1>
 			<br></br>
-			<p class='text-3xl text-blue-200'>Well, its plain</p>
-			<p class='text-3xl text-blue-200'>And simple to express:</p>
+			<p class='text-3xl text-blue-200'>Well, Its Plain</p>
+			<p class='text-3xl text-blue-200'>And Simple To Express:</p>
 			<br></br>
 			<p class='text-3xl text-blue-200'>Err</p>
-			<p class='text-3xl text-blue-200'>And Err</p>
-			<p class='text-3xl text-blue-200'>And Err again</p>
+			<p class='text-3xl text-blue-200'>
+				<span class='ml-[40px]'></span>And Err
+			</p>
+			<p class='text-3xl text-blue-200'>
+				<span class='ml-[40px]'></span>
+				<span class='ml-[40px]'></span>And Err Again.
+			</p>
 			<br></br>
-			<p class='text-3xl text-blue-200'>But less</p>
-			<p class='text-3xl text-blue-200'>and less</p>
-			<p class='text-3xl text-blue-200'>and less.</p>
+
+			<p class='text-3xl text-blue-200'>But Less</p>
+			<p class='text-3xl text-blue-200'>
+				<span class='ml-[40px]'></span>And Less
+			</p>
+			<p class='text-3xl text-blue-200'>
+				<span class='ml-[40px]'></span>
+				<span class='ml-[40px]'></span>And Less.
+			</p>
 			<br></br>
 			<br></br>
 			<p class=' text-blue-200'> ___ </p>
