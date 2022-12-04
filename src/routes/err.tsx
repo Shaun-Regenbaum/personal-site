7e5df65b45a0dc73dd9a77e3be185d0d118012cc
shaunregenbaum <shaunregenbaum@gmail.com>
2022-12-04 14:42:37 +0200

    coolvetica is nice

--- src/routes/err.tsx
+++ src/routes/err.tsx
@@ -1,34 +1,32 @@
 export default function Err() {
-	// the tab special charecter in html:
 	return (
-		<div class='bg-gray-800 h-screen w-full pl-12 pr-4 pt-12 font-serif'>
-			<h1 class='text-blue-200 text-5xl'>What Is The Road To Wisdom?</h1>
+		<div class='bg-black h-screen w-full pl-12 pr-4 pt-12 font-coolvetica tracking-wide text-blue-50 text-3xl'>
+			<h1>What Is The Road To Wisdom?</h1>
 			<br></br>
-			<p class='text-3xl text-blue-200'>Well, Its Plain</p>
-			<p class='text-3xl text-blue-200'>And Simple To Express:</p>
+			<p>Well, Its Plain</p>
+			<p>And Simple To Express:</p>
 			<br></br>
-			<p class='text-3xl text-blue-200'>Err</p>
-			<p class='text-3xl text-blue-200'>
+			<p>Err</p>
+			<p>
 				<span class='ml-[40px]'></span>And Err
 			</p>
-			<p class='text-3xl text-blue-200'>
+			<p>
 				<span class='ml-[40px]'></span>
 				<span class='ml-[40px]'></span>And Err Again.
 			</p>
 			<br></br>
 
-			<p class='text-3xl text-blue-200'>But Less</p>
-			<p class='text-3xl text-blue-200'>
+			<p>But Less</p>
+			<p>
 				<span class='ml-[40px]'></span>And Less
 			</p>
-			<p class='text-3xl text-blue-200'>
+			<p>
 				<span class='ml-[40px]'></span>
 				<span class='ml-[40px]'></span>And Less.
 			</p>
 			<br></br>
-			<br></br>
-			<p class=' text-blue-200'> ___ </p>
-			<p class=' text-blue-200'>Piet Hein</p>
+			<p> _____ </p>
+			<p class='font-pixel'>Piet Hein</p>
 		</div>
 	);
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,13 @@ import Nav from '../components/Nav';
 export default function Home() {
 	return (
 		<div>
-			<Nav />
 			<main class='mx-auto bg-gray-800 h-screen'>
-				<section class='pl-4 pt-4'>
-					<h1 class='font-pixel body-font text-white text-5xl'>
-						I am Shaun Regenbaum
-					</h1>
+				<section class='pl-12 pt-12'>
+					<h1 class='font-coolvetica text-white text-5xl'>Welcome to all!</h1>
+					<br></br>
+					<h2 class='font-coolvetica text-white text-5xl'>I am</h2>
+
+					<h2 class='font-pixel text-white text-3xl'>Shaun Regenbaum</h2>
 				</section>
 			</main>
 		</div>
