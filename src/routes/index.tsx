2dc94ed75ee4363beebd7ac9f456172585e0c5b6
shaunregenbaum <shaunregenbaum@gmail.com>
2022-12-13 23:06:17 +0200

    more stuff

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -25,17 +25,30 @@ export default function Home() {
 					<h3>I now live in Jerusalem, Israel.</h3>
 				</section>
 				<section class='pl-12 pt-12'>
-					<h1>Experience:</h1>
-					<h3>Bioinformatics</h3>
-					<h3>Web Development</h3>
-					<h3>Machine Learning</h3>
-					<h3>Starting Companies</h3>
-					<h3>Embedded Systems</h3>
-
-					<h1>Research:</h1>
+					<p> Paid for college by selling my first business in high school.</p>
+					<p>Started the first funded student-led lab at Georgia Tech.</p>
+					<p>Published first paper in Science.</p>
+					<p>
+						Worked in Nahmias Lab at HU (cultivated meat and tissue-on-a-chip
+						technologies).
+					</p>
+					<p>Worked on population genomics at Georgia Tech.</p>{' '}
+					<p>
+						Founded 402, a VC-Backed company at the end of college, still in
+						business.
+					</p>
+					<p> Like STM {'>'} Arduino</p>
+					<p>Like Julia {'>'} R</p>
+					<p>Like Typescript {'>'} Javascript</p>
+					<p>Like GPT3</p>
+					<p>Like VSCode</p>
+					<p>Like Talmud</p>
+					<p>Don't like driving (waiting for autonomous cars)</p>
+					<p>Don't like disease (working on eliminating it)</p>
+					<p>Don't like hoarding wealth</p>
+					<h1>Education:</h1>
 					<h3>Hebrew University</h3>
 					<h3>Georgia Tech</h3>
-
 					<h1>Awards:</h1>
 					<h3>Zell Miller Scholarship</h3>
 					<h3>Towers Award</h3>
