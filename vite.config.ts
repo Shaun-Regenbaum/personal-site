45db77ab00be14d2a885c20e9c896cd8c1d295a8
shaunregenbaum <shaunregenbaum@gmail.com>
2022-12-14 09:55:00 -0500

    static adapter

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 import staticad from 'solid-start-static';
 
 export default defineConfig({
-	plugins: [solid({ adapter: staticad })],
+	plugins: [solid({ adapter: staticad({}) })],
 });
