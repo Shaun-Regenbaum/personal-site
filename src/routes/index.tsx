d61559653da3781df26eb57aa3a158792c4f5cac
shaunregenbaum <shaunregenbaum@gmail.com>
2022-12-14 07:47:44 -0500

    fonts!

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,26 +14,50 @@ export default function Home() {
 				</section>
 				<section class='pl-16 sm:pl-20 pt-12'>
 					<h3>
-						I am an <span class='text-3xl'>eng</span>
-						<span class='font-steamy text-5xl'>i</span>
-						<span class='text-3xl'>nee</span>
-						<span class='font-steamy text-5xl'>r</span>.
+						My family is from Johanessburg, South Africa.
+						<span class='text-3xl'> 🇿🇦</span>
 					</h3>
 					<h3>
-						I am a <span class='font-science text-3xl'>researcher</span>.
+						I grew up in Atlanta, Georgia.<span class='text-3xl'> 🇺🇸</span>
 					</h3>
 					<h3>
-						At the interface between mind-blowing science and commerical scale.
+						I now live in Jerusalem, Israel.<span class='text-3xl'> 🇮🇱</span>
+					</h3>
+				</section>
+				<section class='pl-16 sm:pl-20 pt-12'>
+					<h3>
+						I am an <span class='font-threeam text-3xl'>engineer.</span>
+					</h3>
+					<h3>
+						And a <span class='font-science text-3xl'>researcher.</span>
 					</h3>
 					<br></br>
-					<h3>My family is from Johanessburg, South Africa.</h3>
-					<h3>I grew up in Atlanta, Georgia.</h3>
-					<h3>I now live in Jerusalem, Israel.</h3>
 				</section>
 				<section class='pl-12 pt-12'>
-					<p> Paid for college by selling my first business in high school.</p>
-					<p>Started the first funded student-led lab at Georgia Tech.</p>
-					<p>Published first paper in Science.</p>
+					<p class='align-bottom'>
+						Paid for <span class='font-threeam'>College</span> by selling my
+						first <span class='font-threeam'>Business</span> in high school.
+					</p>
+					<p>
+						Co-founded the first funded student-led lab at{' '}
+						<span class='font-science text-lg'>Georgia Tech</span>,
+						<div class='pl-2'>
+							<span class='font-aorell'>
+								<div>The</div>
+								<div class='pl-4'>Talmud</div>
+								<div class='pl-12'>
+									Lab,{' '}
+									<span class='font-sans align-bottom'>
+										with my friend Dan Jutan.
+									</span>
+								</div>
+							</span>
+						</div>
+					</p>
+					<p>
+						Published my first paper in{' '}
+						<span class='font-science text-lg'>Science</span>.
+					</p>
 					<p>
 						Worked in Nahmias Lab at HU (cultivated meat and tissue-on-a-chip
 						technologies).
@@ -43,7 +67,12 @@ export default function Home() {
 						Founded 402, a VC-Backed company at the end of college, still in
 						business.
 					</p>
-					<p> Like STM {'>'} Arduino</p>
+				</section>
+				<section class='pl-12 pt-12'>
+					<p>
+						{' '}
+						Like <span class='font-steamy text-xl'>S T M</span> {'>'} Arduino
+					</p>
 					<p>Like Julia {'>'} R</p>
 					<p>Like Typescript {'>'} Javascript</p>
 					<p>Like GPT3</p>
@@ -52,6 +81,8 @@ export default function Home() {
 					<p>Don't like driving (waiting for autonomous cars)</p>
 					<p>Don't like disease (working on eliminating it)</p>
 					<p>Don't like hoarding wealth</p>
+				</section>
+				<section class='pl-12 pt-12'>
 					<h1>Education:</h1>
 					<h3>Hebrew University</h3>
 					<h3>Georgia Tech</h3>
