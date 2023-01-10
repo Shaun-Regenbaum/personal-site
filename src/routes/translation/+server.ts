e08db0b7b2be87c2a74a3099499bfa4194c12a03
shaunregenbaum <shaunregenbaum@gmail.com>
2023-01-10 10:46:23 +0200

    added translation

---
+++ src/routes/translation/+server.ts
@@ -0,0 +1,11 @@
+import { json } from '@sveltejs/kit';
+import { createTranslation } from '$lib/openai';
+
+export async function POST({ request }: any) {
+	const debug: boolean = true;
+	const body = await request.json();
+	const { aramaic, english, word } = body;
+
+	const translation = await createTranslation(aramaic, english, word, debug);
+	return json(translation);
+}
